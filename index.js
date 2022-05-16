1c8c8e1ffff0eb5b665f0ad263da84f10e3c4162
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-16 17:37:39 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  'mongodb+srv://doctor_admin:<password>@cluster0.xjrle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 app.get('/', (req, res) => {
   res.send('Hello From Doctors Portals!');
