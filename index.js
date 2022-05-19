bc768976ad43fad8a747fb925813c5c685bf25ef
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-19 08:59:30 +0600

    create post api for booking data

--- index.js
+++ index.js
@@ -20,12 +20,28 @@ async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services');
+    const bookingCollection = client.db('doctors_portal').collection('bookings');
 
     app.get('/service', async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.toArray();
       res.send(services);
+    });
+
+    /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking
+     * app.post('/booking') // add a new booking
+     * app.patch('booking/:id') //
+     * app.delete('/booking/:id) //
+     */
+
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
     });
   } finally {
     // some type of code that stop code.
