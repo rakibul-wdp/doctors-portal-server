885ea35a5f0e9b819021422d4bb28a79e9e7da4e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-05-19 18:09:03 +0600

    add jwt and genarate access token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
@@ -39,7 +40,8 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ result, token });
     });
 
     // Warning:
